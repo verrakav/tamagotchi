ed472122df42dbc8c430211953ea697354da80af
verrakav <verrakav@users.noreply.github.com>
2024-08-16 22:19:04 +1000

    working btns & changing indicators' content

--- main.js
+++ main.js
@@ -4,17 +4,53 @@ const btnPlay = document.getElementById("btn-play");
 const btnTrain = document.getElementById("btn-train");
 
 //select indicators
-const indHunger = document.getElementById("hunger-indicator");
-const indEnergy = document.getElementById("energy-indicator");
-const indSkill = document.getElementById("skills-indicator");
-const indAge = document.getElementById("age-indicator");
+let indHunger = document.getElementById("hunger-indicator");
+let indEnergy = document.getElementById("energy-indicator");
+let indSkill = document.getElementById("skills-indicator");
+let indAge = document.getElementById("age-indicator");
+
+//initial setup NOTE: should be in my pet object?
+let hunger = 2;
+let energy = 100;
+let skill = 0;
+let age = 0;
+
+//define handlers
+const handleFeed = () => {
+  hunger--;
+  hunger < 0 && (hunger = 0);
+  indHunger.innerHTML = hunger;
+};
+
+const handlePlay = () => {
+  energy--;
+  energy < 0 && (energy = 0);
+  indEnergy.innerHTML = energy;
+};
+
+const handleTrain = () => {
+  skill++;
+  indSkill.innerHTML = skill;
+};
+
 //hook em up to the indicators
 btnFeed.addEventListener("click", e => handleFeed());
 
 btnPlay.addEventListener("click", e => handlePlay());
 
 btnTrain.addEventListener("click", e => handleTrain());
-//separate file class Pet()
+
+//handle age with an interval
+const countAge = () => {
+  setInterval(() => {
+    age++;
+    indAge.innerHTML = age + " y.o.";
+    console.log(age);
+  }, 5000);
+};
+countAge();
+
+//separate file class Pet()?
 
 //NOTE: till later?
 //make the start page + logic/elms to choose pets
@@ -27,4 +63,4 @@ btnTrain.addEventListener("click", e => handleTrain());
 // module.exports = sum;
 
 //TO DO:
-//
+//think through the logic of scoring
